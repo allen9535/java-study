882a978a6fd26eb1d07b9e3d65392fedea3d66e5
allen9535 <allen9535@naver.com>
2022-01-05 22:05:36 +0900

    Part 2. 객체지향 프로그래밍

---
+++ Chapter2/src/ch10/Person.java
@@ -0,0 +1,51 @@
+package ch10;
+
+public class Person {
+
+  String name;
+  int age;
+
+  public Person() {
+
+    // this의 역할: 생성자에서 또 다른 생성자를 호출한다.
+    // 이 경우, 인스턴스 생성이 완전히 가능한 상태가 아니므로,
+    // this 구문 이전에 다른 코드를 작성할 수 없다. 
+    this("이름 없음", 1);
+
+  }
+
+  public Person(String name, int age) {
+
+    // this의 역할: 인스턴스 자신의 메모리를 가리킨다.
+    this.name = name;
+    this.age = age;
+
+  }
+
+  public void showPerson() {
+
+    System.out.printf("%s, %d\n", name, age);
+
+  }
+
+  public Person getPerson() {
+
+    // this의 역할: 자신의 주소(참조값)을 반환한다.
+    return this;
+
+  }
+
+  public static void main(String[] args) {
+
+    Person p = new Person();
+    p.showPerson();
+    p.name = "James";
+    p.age = 37;
+    System.out.println(p);
+
+    Person p2 = p.getPerson();
+    System.out.println(p2);
+
+  }
+
+}
