9b9978f37e3a48e1fa08960c7b20b898a49fa85a
allen9535 <allen9535@naver.com>
2022-01-12 15:56:17 +0900

    Part 2. 객체지향 프로그래밍

---
+++ Chapter2/src/ch17/Car.java
@@ -0,0 +1,14 @@
+package ch17;
+
+public class Car {
+
+  private int carNum;
+
+  public int getCarNum() {
+    return carNum;
+  }
+  public void setCarNum(int carNum) {
+    this.carNum = carNum;
+  }
+
+}
