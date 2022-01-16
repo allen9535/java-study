1df1d3cd06769ccafcf2d4f001bdd1516af79ac8
allen9535 <allen9535@naver.com>
2022-01-16 18:58:02 +0900

    Part 2. 객체지향 프로그래밍 - 객체 지향 핵심

---
+++ Chapter3/src/ch01/Inheritance.java
@@ -0,0 +1,23 @@
+package ch01;
+
+public class Inheritance {
+
+  // 클래스 상속
+  // 새로운 클래스를 정의할 때 이미 구현된 클래스를 상속(inheritance)받아,
+  // 속성이나 기능을 확장해 클래스를 구성한다.
+  // 이미 구현된 클래스보다 더 구체적인 기능을 가진 클래스를 구현할 때 기존 클래스를 상속한다.
+
+  // 상속하는 클래스: 상위 클래스, parent class, base class, super class
+  // 상속받는 클래스: 하위 클래스, child class, derived class, subclass
+
+  // 문법
+  // class B extends A { }
+  // extends 키워드 뒤에는 단 하나의 클래스만 올 수 있다.
+  // 자바는 단일 상속(single inheritance)만 지원한다.
+
+  // 상속을 구현하는 경우
+  // 상위 클래스는 하위 클래스에 비해 더 일반적인 개념과 기능을 가진다.
+  // 하위 클래스는 상위 클래스에 비해 더 구체적인 개념과 기능을 가진다.
+  // 하위 클래스가 상위 클래스의 속성, 기능을 확장(extends)한다고 생각하면 된다.
+
+}
