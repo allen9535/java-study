91b3ca7f33b493174149035af17ef7a97d02de63
allen9535 <allen9535@naver.com>
2022-01-27 14:28:01 +0900

    Part 2. 객체지향 프로그래밍 - 객체 지향 핵심

---
+++ Chapter3/src/ch13/Define.java
@@ -0,0 +1,16 @@
+package ch13;
+
+public class Define {
+
+  // final 예약어
+  // final 변수: 값이 변경될 수 없는 상수
+  // final 메서드: 하위 클래스에서 재정의 할 수 없는 메서드
+  // final 클래스: 상속할 수 없는 클래스
+  public static final int MIN = 1;
+  public static final int MAX = 999999;
+  public static final double PI = 3.14;
+  public static final String GREETING = "Good Morning!";
+  public static final int MATH_CODE = 1001;
+  public static final int CHEMISTRY_CODE = 1002;
+
+}
